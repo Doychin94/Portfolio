c38fffa873c5076ce7eb4a12b3eff22f62101350
ddarakchiev <ddarakchiev@htmlbox.net>
2024-03-12 19:20:10 +0200

    Add progress bar

--- src/js/app.js
+++ src/js/app.js
@@ -3,11 +3,22 @@
 const $header = $('.js-header');
 
 
-$(window).on('load scroll resize', function(evt) {
+$(window).on('load scroll resize', function() {
 	if ($(this).scrollTop() > 0) {
 		$header.addClass('is-sticky');
 		return;
 	};
 
 	$header.removeClass('is-sticky')
+})
+
+
+$('.js-bar').each((index, bar) => {
+	const $barScore = $(bar).find('.bar__score');
+	const $barLabel = $(bar).find('.bar__label');
+	const score = $barScore.data('percent');
+
+	$barScore.css({'width': `${score}%`});
+
+	$barLabel.html(`${score}%`)
 })
